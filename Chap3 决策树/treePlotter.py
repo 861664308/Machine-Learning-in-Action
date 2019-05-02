7b59c040d3764e68503c8ea0f18a33d9e0a51868
861664308 <861664308@qq.com>
2019-05-03 00:22:00 +0800

    第三章

--- Chap3 决策树/treePlotter.py
+++ Chap3 决策树/treePlotter.py
@@ -7,7 +7,6 @@ arrow_args = dict(arrowstyle = '<-') #设置箭头格式
 
 def plotNode(nodeTex, centerPt, parentPt, nodeType):
     '''
-
     :param nodeTex: 要显示的文本
     :param centerPt: 文本的中心点
     :param parentPt: 指向文本的点
@@ -18,15 +17,23 @@ def plotNode(nodeTex, centerPt, parentPt, nodeType):
                 xytext = centerPt, textcoords = 'axes fraction', va = 'center',
                 ha = 'center', bbox = nodeType, arrowprops = arrow_args)
 
-def createPlot():
+def createPlot(inTree):
     #创建fig
     fig = plt.figure(1, facecolor='white')
     #清空fig
     fig.clf()
+    axprops = dict(xticks = [], yticks = [])
     #去掉x，y轴
-    createPlot.ax1 = plt.subplot(111, frameon = False)
-    plotNode('决策节点', (0.5, 0.1), (0.1, 0.5), dicisionNode)
-    plotNode('叶节点', (0.8, 0.1), (0.3, 0.8), leafNode)
+    createPlot.ax1 = plt.subplot(111, frameon = False, **axprops)
+    # 获取叶节点个数
+    plotTree.totalW = float(getNumLeaf(inTree))
+    # 获取层数
+    plotTree.totalD = float(getTreeDepth(inTree))
+    # x偏移与y偏移
+    plotTree.xOff = -0.5 / plotTree.totalW
+    plotTree.yOff = 1.0
+    #绘制决策树
+    plotTree(inTree, (0.5, 1.0), '')
     plt.show()
 #获取树的节点
 def getNumLeaf(myTree):
@@ -80,3 +87,38 @@ def retrieveTree(i):
         {0 : {'head' : {0 : 'no', 1 : 'yes'}}, 1 : 'no'
          }}}}]
     return listOfTrees[i]
+
+def plotMidText(cntrpt, parentpt, txtString):
+    #计算标注位置
+    xMid = (parentpt[0] - cntrpt[0]) / 2.0 +cntrpt[0]
+    yMid = (parentpt[1] - cntrpt[1]) / 2.0 + cntrpt[1]
+    createPlot.ax1.text(xMid, yMid, txtString)
+
+def plotTree(myTree, parentPt, nodeTxt):
+    #获取叶节点的数目，决定树的宽度
+    numLeafs = getNumLeaf(myTree)
+    #获取决策树层数
+    depth = getTreeDepth(myTree)
+    #下个字典
+    firstStr = next(iter(myTree))
+    #中心位置
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs)) / 2.0/plotTree.totalW, plotTree.yOff)
+    # 标注有向边属性值
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    # 绘制结点
+    plotNode(firstStr, cntrPt, parentPt, dicisionNode)
+    # 下一个字典，也就是继续绘制结点
+    secondDict = myTree[firstStr]
+    # y偏移
+    plotTree.yOff = plotTree.yOff - 1.0 / plotTree.totalD
+    for key in secondDict.keys():
+        # 测试该结点是否为字典，如果不是字典，代表此结点为叶子结点
+        if type(secondDict[key]).__name__ == 'dict':
+            # 不是叶结点，递归调用继续绘制
+            plotTree(secondDict[key], cntrPt, str(key))
+        else:
+            # 如果是叶结点，绘制叶结点，并标注有向边属性值
+            plotTree.xOff = plotTree.xOff + 1.0 / plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+        plotTree.yOff = plotTree.yOff + 1.0 / plotTree.totalD
